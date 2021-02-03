b9edae73c2c3eb217bfad2280df03ddd1215a2fb
mice85 <miguelangel.munoz@ua.es>
2021-02-03 17:44:07 +0100

    new parameters configured, and new function for objects detection

--- local_planning/src/local_planning_alg_node.cpp
+++ local_planning/src/local_planning_alg_node.cpp
@@ -43,6 +43,11 @@ LocalPlanningAlgNode::LocalPlanningAlgNode(void) :
                                      filter_config_.min_dot_product_for_ground);
   this->public_node_handle_.getParam("/filter_configuration/max_ground_elevation",
                                      filter_config_.max_ground_elevation_angle_change_in_degrees);
+  this->public_node_handle_.getParam("/filter_configuration/a", filter_config_.a);
+  this->public_node_handle_.getParam("/filter_configuration/b", filter_config_.b);
+  this->public_node_handle_.getParam("/filter_configuration/c", filter_config_.c);
+  this->public_node_handle_.getParam("/filter_configuration/variance", filter_config_.variance);
+  this->public_node_handle_.getParam("/filter_configuration/radious", filter_config_.radious);
 
   this->public_node_handle_.getParam("/lidar_configuration/max_elevation_angle", lidar_config_.max_elevation_angle);
   this->public_node_handle_.getParam("/lidar_configuration/min_elevation_angle", lidar_config_.min_elevation_angle);
@@ -142,8 +147,8 @@ void LocalPlanningAlgNode::cb_lidarInfo(const sensor_msgs::PointCloud2::ConstPtr
     pcl_conversions::toPCL(*scan, scan_pcl2);
     pcl::fromPCLPointCloud2(scan_pcl2, scan_pcl);
 
-    this->local_planning_->freeSpaceMap(scan_pcl, this->lidar_config_, this->filter_config_, scan_pcl_filt,
-                                        free_space_pcl);
+    this->local_planning_->groundSegmentation(scan_pcl, this->lidar_config_, this->filter_config_, scan_pcl_filt,
+                                              free_space_pcl);
 
     pcl::toPCLPointCloud2(scan_pcl_filt, scan_pcl2);
     pcl_conversions::fromPCL(scan_pcl2, scan_filt);
@@ -159,43 +164,42 @@ void LocalPlanningAlgNode::cb_lidarInfo(const sensor_msgs::PointCloud2::ConstPtr
     //////////////////////////////////////////////////
     //// potential forces map calculation
     // size map calculation
-    float max_x = 0.0, min_x = 0.0, max_y = 0.0, min_y = 0.0;
-    int max_x_int, min_x_int, max_y_int, min_y_int;
-    for (int i = 0; i < free_space_pcl.points.size(); ++i)
-    {
-      if (free_space_pcl.points[i].x > max_x)
-      {
-        max_x = free_space_pcl.points[i].x;
-      }
-      if (free_space_pcl.points[i].x < min_x)
-      {
-        min_x = free_space_pcl.points[i].x;
-      }
-      if (free_space_pcl.points[i].y > max_y)
-      {
-        max_y = free_space_pcl.points[i].y;
-      }
-      if (free_space_pcl.points[i].y < min_y)
-      {
-        min_y = free_space_pcl.points[i].y;
-      }
-    }
-    max_x_int = (int)(max_x * this->pf_config_.scale);
-    min_x_int = (int)(min_x * this->pf_config_.scale);
-    max_y_int = (int)(max_y * this->pf_config_.scale);
-    min_y_int = (int)(min_y * this->pf_config_.scale);
-    this->pf_config_.size_x = max_x_int - min_x_int + 1;
-    this->pf_config_.size_y = max_y_int - min_y_int + 1;
-    this->pf_config_.offset_x = -min_x_int;
-    this->pf_config_.offset_y = -min_y_int;
+    /*float max_x = 0.0, min_x = 0.0, max_y = 0.0, min_y = 0.0;
+     int max_x_int, min_x_int, max_y_int, min_y_int;
+     for (int i = 0; i < free_space_pcl.points.size(); ++i)
+     {
+     if (free_space_pcl.points[i].x > max_x)
+     {
+     max_x = free_space_pcl.points[i].x;
+     }
+     if (free_space_pcl.points[i].x < min_x)
+     {
+     min_x = free_space_pcl.points[i].x;
+     }
+     if (free_space_pcl.points[i].y > max_y)
+     {
+     max_y = free_space_pcl.points[i].y;
+     }
+     if (free_space_pcl.points[i].y < min_y)
+     {
+     min_y = free_space_pcl.points[i].y;
+     }
+     }
+     max_x_int = (int)(max_x * this->pf_config_.scale);
+     min_x_int = (int)(min_x * this->pf_config_.scale);
+     max_y_int = (int)(max_y * this->pf_config_.scale);
+     min_y_int = (int)(min_y * this->pf_config_.scale);
+     this->pf_config_.size_x = max_x_int - min_x_int + 1;
+     this->pf_config_.size_y = max_y_int - min_y_int + 1;
+     this->pf_config_.offset_x = -min_x_int;
+     this->pf_config_.offset_y = -min_y_int;
 
-    //vector<vector<cv::Point> > contour_plt;
-    //cv::Mat pf_map(this->pf_config_.size_y, this->pf_config_.size_x, CV_8UC3, EMPTY_PIXEL);
-    //cv::Mat pf_map_plt(this->pf_config_.size_y, this->pf_config_.size_x, CV_8UC3, EMPTY_PIXEL);
-    //this->alg_.potentialForcesMap(free_space_pcl, this->goal_lidar_, this->pf_config_, contour_plt, pf_map);
-    //cv::applyColorMap(pf_map, pf_map_plt, cv::COLORMAP_JET);
+     //vector<vector<cv::Point> > contour_plt;
+     //cv::Mat pf_map(this->pf_config_.size_y, this->pf_config_.size_x, CV_8UC3, EMPTY_PIXEL);
+     //cv::Mat pf_map_plt(this->pf_config_.size_y, this->pf_config_.size_x, CV_8UC3, EMPTY_PIXEL);
+     //this->alg_.potentialForcesMap(free_space_pcl, this->goal_lidar_, this->pf_config_, contour_plt, pf_map);
+     //cv::applyColorMap(pf_map, pf_map_plt, cv::COLORMAP_JET);*/
     //////////////////////////////////////////////////
-
     //////////////////////////////////////////////////
     //// ROAD MAP CALCULATION
     //cv::Mat roads_map(this->pf_config_.size_y, this->pf_config_.size_x, CV_8UC3, EMPTY_PIXEL);
@@ -203,65 +207,61 @@ void LocalPlanningAlgNode::cb_lidarInfo(const sensor_msgs::PointCloud2::ConstPtr
     //this->alg_.findTransitableAreas(pf_map, contour_plt, this->goal_lidar_, this->pf_config_, roads_map);
     //cv::applyColorMap(roads_map, roads_map_plot, cv::COLORMAP_JET);
     //////////////////////////////////////////////////
-
     ini2 = ros::Time::now().toSec();
 
     //////////////////////////////////////////////////
     //// NAIVE GEODESIC PATH CALCULATION
-    cv::Mat plot_img(this->pf_config_.size_y, this->pf_config_.size_x, CV_8UC1, EMPTY_PIXEL);
-    vector < vector<cv::Point> > contour;
-    vector < cv::Point2d > goal_candidates;
-    cv::Point2d local_goal;
-    int radious;
-    this->alg_.findLocalGoal(free_space_pcl, this->goal_lidar_, this->pf_config_, contour, radious, plot_img,
-                             goal_candidates, local_goal);
+    /*cv::Mat plot_img(this->pf_config_.size_y, this->pf_config_.size_x, CV_8UC1, EMPTY_PIXEL);
+     vector < vector<cv::Point> > contour;
+     vector < cv::Point2d > goal_candidates;
+     cv::Point2d local_goal;
+     int radious;
+     this->alg_.findLocalGoal(free_space_pcl, this->goal_lidar_, this->pf_config_, contour, radious, plot_img,
+     goal_candidates, local_goal);
 
-    //plot
-    cv::Point2d uv, uv2;
-    uv.x = this->pf_config_.offset_x;
-    uv.y = this->pf_config_.offset_y;
-    cv::circle(plot_img, uv, radious, CV_RGB(MAX_PIXEL, EMPTY_PIXEL, EMPTY_PIXEL), 1);
-    for (int i = 0; i < goal_candidates.size(); i++)
-    {
-      cv::circle(plot_img, goal_candidates[i], 1, CV_RGB(EMPTY_PIXEL, EMPTY_PIXEL, MAX_PIXEL), -1);
-    }
-    cv::circle(plot_img, local_goal, 2, CV_RGB(EMPTY_PIXEL, MAX_PIXEL, EMPTY_PIXEL), -1);
+     //plot
+     cv::Point2d uv, uv2;
+     uv.x = this->pf_config_.offset_x;
+     uv.y = this->pf_config_.offset_y;
+     cv::circle(plot_img, uv, radious, CV_RGB(MAX_PIXEL, EMPTY_PIXEL, EMPTY_PIXEL), 1);
+     for (int i = 0; i < goal_candidates.size(); i++)
+     {
+     cv::circle(plot_img, goal_candidates[i], 1, CV_RGB(EMPTY_PIXEL, EMPTY_PIXEL, MAX_PIXEL), -1);
+     }
+     cv::circle(plot_img, local_goal, 2, CV_RGB(EMPTY_PIXEL, MAX_PIXEL, EMPTY_PIXEL), -1);*/
     //////////////////////////////////////////////////
-
     //////////////////////////////////////////////////
     //// CONTROL ACTIONS
-    static float speed_prev = 0.0;
-    this->alg_.findControlAction(free_space_pcl, local_goal, this->base_in_lidarf_, this->goal_lidar_, this->pf_config_,
-                                 this->ctrl_config_, contour, this->ackermann_state_, plot_img);
+    /*static float speed_prev = 0.0;
+     this->alg_.findControlAction(free_space_pcl, local_goal, this->base_in_lidarf_, this->goal_lidar_, this->pf_config_,
+     this->ctrl_config_, contour, this->ackermann_state_, plot_img);
 
-    //proportional filtration
-    if (this->flag_prop_)
-    {
-      this->ackermann_state_.drive.speed = speed_prev + (this->ackermann_state_.drive.speed - speed_prev) * this->kp_;
-      speed_prev = this->ackermann_state_.drive.speed;
-    }
-    this->ackermann_state2_ = this->ackermann_state_;
-    this->ackermann_state2_.drive.steering_angle = this->ackermann_state_.drive.steering_angle * 180.0 / PI;
-    this->ctrl_received_ = true;
+     //proportional filtration
+     if (this->flag_prop_)
+     {
+     this->ackermann_state_.drive.speed = speed_prev + (this->ackermann_state_.drive.speed - speed_prev) * this->kp_;
+     speed_prev = this->ackermann_state_.drive.speed;
+     }
+     this->ackermann_state2_ = this->ackermann_state_;
+     this->ackermann_state2_.drive.steering_angle = this->ackermann_state_.drive.steering_angle * 180.0 / PI;
+     this->ctrl_received_ = true;*/
     //////////////////////////////////////////////////
-
     ///////////////////////////////////////////////////
     //// PLOT AND FILE OUTPUT
-    std_msgs::Header header; // empty header
-    header.stamp = ros::Time::now(); // time
-    cv_bridge::CvImage output_bridge;
-    output_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::BGR8, plot_img);
-    this->plot_publisher_.publish(output_bridge.toImageMsg());
-    if (this->save_map_)
-    {
-      static int cont = 0;
-      std::ostringstream out_path_map;
-      out_path_map << this->out_path_map_ << cont << ".jpg";
-      cv::imwrite(out_path_map.str(), plot_img);
-      cont++;
-    }
+    /*std_msgs::Header header; // empty header
+     header.stamp = ros::Time::now(); // time
+     cv_bridge::CvImage output_bridge;
+     output_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::BGR8, plot_img);
+     this->plot_publisher_.publish(output_bridge.toImageMsg());
+     if (this->save_map_)
+     {
+     static int cont = 0;
+     std::ostringstream out_path_map;
+     out_path_map << this->out_path_map_ << cont << ".jpg";
+     cv::imwrite(out_path_map.str(), plot_img);
+     cont++;
+     }*/
     ///////////////////////////////////////////////////
-
   }
 
   // loop time
@@ -363,9 +363,11 @@ void LocalPlanningAlgNode::node_config_update(Config &config, uint32_t level)
 {
   this->alg_.lock();
   this->config_ = config;
-  this->config_.a;
-  this->config_.b;
-  this->config_.c;
+  this->filter_config_.a = this->config_.a;
+  this->filter_config_.b = this->config_.b;
+  this->filter_config_.c = this->config_.c;
+
+  this->filter_config_.variance = this->config_.variance;
 
   this->alg_.unlock();
 }
